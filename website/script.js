7d9e15c2faf7b098fe49394f24dfa1a5b7b477b0
williamLyh <512272913@qq.com>
2023-04-16 01:31:34 +0100

    game

--- website/script.js
+++ website/script.js
@@ -1,131 +1,224 @@
-class Game2048 {
-    constructor(boardElement) {
-        this.boardElement = boardElement;
-        this.board = this.initBoard();
-        this.addRandomTile();
-        this.addRandomTile();
-        this.render();
-        this.listen();
+let board = [
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', '']
+  ];
+  
+  let score = 0;
+  let gameover = false;
+  
+  function newGame() {
+    board = [
+      ['', '', '', ''],
+      ['', '', '', ''],
+      ['', '', '', ''],
+      ['', '', '', '']
+    ];
+    score = 0;
+    gameover = false;
+    addTile();
+    addTile();
+    updateBoard();
+  }
+  
+  function addTile() {
+    let options = [];
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (board[i][j] === '') {
+          options.push({ x: i, y: j });
+        }
+      }
     }
-
-    initBoard() {
-        const board = new Array(4);
-        for (let i = 0; i < 4; i++) {
-            board[i] = new Array(4).fill(0);
-        }
-        return board;
+    if (options.length > 0) {
+      let spot = options[Math.floor(Math.random() * options.length)];
+      let val = Math.random() < 0.9 ? 2 : 4;
+      board[spot.x][spot.y] = val;
     }
-
-    addRandomTile() {
-        const emptyCells = this.getEmptyCells();
-        if (emptyCells.length > 0) {
-            const [row, col] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-            this.board[row][col] = Math.random() < 0.9 ? 2 : 4;
-        }
+  }
+  
+  function slide(row) {
+    let arr = row.filter(val => val);
+    let missing = 4 - arr.length;
+    let zeros = Array(missing).fill('');
+    arr = zeros.concat(arr);
+    return arr;
+  }
+  
+  function combine(row) {
+    for (let i = 3; i >= 1; i--) {
+      let a = row[i];
+      let b = row[i - 1];
+      if (a == b) {
+        row[i] = a + b;
+        score += row[i];
+        row[i - 1] = '';
+      }
     }
-
-    getEmptyCells() {
-        const emptyCells = [];
-        for (let row = 0; row < 4; row++) {
-            for (let col = 0; col < 4; col++) {
-                if (this.board[row][col] === 0) {
-                    emptyCells.push([row, col]);
-                }
-            }
-        }
-        return emptyCells;
+    return row;
+  }
+  
+  function operate(row) {
+    row = slide(row);
+    row = combine(row);
+    row = slide(row);
+    return row;
+  }
+  
+  function copyBoard(board) {
+    let newBoard = [];
+    for (let i = 0; i < 4; i++) {
+      newBoard.push(board[i].slice());
     }
-
-    render() {
-        this.boardElement.innerHTML = '';
-        for (let row = 0; row < 4; row++) {
-            for (let col = 0; col < 4; col++) {
-                const value = this.board[row][col];
-                const tile = document.createElement('div');
-                if (value !== 0) {
-                    tile.textContent = value;
-                    tile.className = 'tile';
-                    tile.dataset.value = value;
-                }
-                this.boardElement.appendChild(tile);
-            }
-        }
+    return newBoard;
+  }
+  
+  function flipBoard(board) {
+    for (let i = 0; i < 4; i++) {
+      board[i].reverse();
     }
-
-    listen() {
-        document.addEventListener('keydown', (e) => {
-            const keyMap = {
-                ArrowUp: 'up',
-                ArrowDown: 'down',
-                ArrowLeft: 'left',
-                ArrowRight: 'right',
-            };
-            const direction = keyMap[e.key];
-            if (direction) {
-                const moved = this.move(direction);
-                if (moved) {
-                    this.addRandomTile();
-                    this.render();
-                }
-            }
-        });
+    return board;
+  }
+  
+  function transposeBoard(board) {
+    let newBoard = copyBoard(board);
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        newBoard[i][j] = board[j][i];
+      }
     }
-
-    move(direction) {
-        const boardBeforeMove = JSON.stringify(this.board);
-        if (direction === 'up' || direction === 'down') {
-            this.transposeBoard();
+    return newBoard;
+  }
+  
+  function moveRight() {
+    board.forEach(row => {
+      row = operate(row);
+    });
+    addTile();
+    updateBoard();
+  }
+  
+  function moveLeft() {
+    board = flipBoard(board);
+    board.forEach(row => {
+      row = operate(row);
+    });
+    board = flipBoard(board);
+    addTile();
+    updateBoard();
+  }
+  
+  function moveDown() {
+    board = transposeBoard(board);
+    board.forEach(row => {
+      row = operate(row);
+    });
+    board = transposeBoard(board);
+    addTile();
+    updateBoard();
+  }
+  
+  function moveUp() {
+    board = transposeBoard(board);
+    board = flipBoard(board);
+    board.forEach(row => {
+      row = operate(row);
+    });
+    board = flipBoard(board);
+    board = transposeBoard(board);
+    addTile();
+    updateBoard();
+  }
+  
+  function checkWin() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (board[i][j] === 2048) {
+          return true;
         }
-        if (direction === 'right' || direction === 'down') {
-            this.reverseBoard();
-        }
-        for (let row = 0; row < 4; row++) {
-            this.board[row] = this.collapseRow(this.board[row]);
-        }
-        if (direction === 'right' || direction === 'down') {
-            this.reverseBoard();
-        }
-        if (direction === 'up' || direction === 'down') {
-            this.transposeBoard();
-        }
-        const boardAfterMove = JSON.stringify(this.board);
-        return boardBeforeMove !== boardAfterMove;
+      }
     }
-
-    transposeBoard() {
-        const transposed = this.initBoard();
-        for (let row = 0; row < 4; row++) {
-            for (let col = 0; col < 4; col++) {
-                transposed[row][col] = this.board[col][row];
-            }
+    return false;
+  }
+  
+  function updateBoard() {
+    let tiles = document.querySelectorAll('.tile');
+    tiles.forEach(tile => {
+      tile.parentNode.removeChild(tile);
+    });
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        let tile = document.createElement('div');
+        let val = board[i][j];
+        tile.classList.add('tile');
+        tile.setAttribute('data-value', val);
+        if (val !== '') {
+          tile.innerText = val;
         }
-        this.board = transposed;
+        let boardEl = document.querySelector('.board');
+        boardEl.appendChild(tile);
+      }
     }
-
-    reverseBoard() {
-        for (let row = 0; row < 4; row++) {
-            this.board[row] = this.board[row].reverse();
+    let scoreEl = document.querySelector('.score');
+    scoreEl.innerText = `Score: ${score}`;
+    if (gameover) {
+      let gameoverEl = document.querySelector('.gameover');
+      gameoverEl.style.display = 'block';
+    }
+  }
+  
+  function isGameOver() {
+    let emptySpots = 0;
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (board[i][j] === '') {
+          emptySpots++;
         }
-    }
-
-    collapseRow(row) {
-        const newRow = row.filter(val => val !== 0);
-        const zeros = new Array(4 - newRow.length).fill(0);
-        const collapsed = newRow.concat(zeros);
-        for (let i = 0; i < collapsed.length - 1; i++) {
-            if (collapsed[i] === collapsed[i + 1]) {
-                collapsed[i] *= 2;
-                collapsed.splice(i + 1, 1);
-                collapsed.push(0);
-            }
+        if (board[i][j] === 2048) {
+          return true;
         }
-        return collapsed;
+        if (j < 3 && board[i][j] === board[i][j + 1]) {
+          return false;
+        }
+        if (i < 3 && board[i][j] === board[i + 1][j]) {
+          return false;
+        }
+      }
     }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const boardElement = document.getElementById('board');
-    new Game2048(boardElement);
-});
-
-
+    if (emptySpots === 0) {
+      return true;
+    }
+    return false;
+  }
+  
+  newGame();
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') {
+      moveRight();
+    } else if (event.key === 'ArrowLeft') {
+      moveLeft();
+    } else if (event.key === 'ArrowDown') {
+      moveDown();
+    } else if (event.key === 'ArrowUp') {
+      moveUp();
+    }
+  });
+  
+  let newGameButton = document.querySelector('.new-game');
+  newGameButton.addEventListener('click', function() {
+    let gameoverEl = document.querySelector('.gameover');
+    gameoverEl.style.display = 'none';
+    newGame();
+  });
+  
+  let boardEl = document.querySelector('.board');
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let cell = document.createElement('div');
+      cell.classList.add('cell');
+      boardEl.appendChild(cell);
+    }
+  }
+  
